e765830ba7d8b5e83ca112eaae61cf87be735bfa
freestyleweb <elvud@ukr.net>
2020-08-27 10:42:05 +0300

    final pz3

--- lesson_18/consoleApp_PZ_3.js
+++ lesson_18/consoleApp_PZ_3.js
@@ -12,6 +12,8 @@ P.S. Функции вызывать не обязательно */
 
 let numberOfFilms;
 
+
+
 function start() {
     numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
@@ -22,13 +24,17 @@ function start() {
 
 start();
 
+
+
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
+    privat: true
 };
+
+
 
 function rememberMyFilms() {
     for (let i = 0; i < 2; i++) {
@@ -52,15 +58,42 @@ function rememberMyFilms() {
     }
 }
 
+rememberMyFilms();
 
-if (personalMovieDB.count < 10) {
-    console.log('Просмотрено довольно мало фильмов');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30) {
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла ошибка');
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
 }
 
-console.log(personalMovieDB);
+detectPersonalLevel();
+
+
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+    }
+}
+
+writeYourGenres();
+
+
+// console.log(personalMovieDB);
